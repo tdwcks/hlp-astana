2f382769bb4b1019fe21f9d309375935bddab9db
JamesHanks <james.ra.hanks@gmail.com>
2017-08-09 14:28:09 +0100

    Risks Failed - Return to Normal

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -53,7 +53,7 @@ $( ".js-scroll-down-button" ).on( "click", function() {
 
 $( ".js-learn-more" ).on( "click", function() {
 	$(this).toggleClass("active");
-	$(this).toggleText(<%= I18n.t(:information_cta_1) %>, <%= I18n.t(:information_cta_2) %>);
+	$(this).toggleText('Show More', 'Show Less');
 	$(".how-to-play").toggleClass("active");
 	$(".what-to-expect").toggleClass("active");
 	$('html, body').animate({
