7a5f1a82c7ba2e673b47113a4bcb43d60025e511
JamesHanks <james.ra.hanks@gmail.com>
2017-08-09 14:17:34 +0100

    Risky JS Changes for Show More/Less

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -53,7 +53,7 @@ $( ".js-scroll-down-button" ).on( "click", function() {
 
 $( ".js-learn-more" ).on( "click", function() {
 	$(this).toggleClass("active");
-	$(this).toggleText('Show More', 'Show Less');
+	$(this).toggleText('<%= I18n.t(:information_cta_1) %>', '<%= I18n.t(:information_cta_2) %>');
 	$(".how-to-play").toggleClass("active");
 	$(".what-to-expect").toggleClass("active");
 	$('html, body').animate({
